ef98db33391d28d7ba3c5e51a750f5562d87485a
Keith Fung <fungsenkeith@gmail.com>
2020-10-21 20:07:30 -0400

    Redo

--- Develop/index.js
+++ Develop/index.js
@@ -1,110 +1,79 @@
-const fs = require('fs');
-const util = require('util');
+const fs = require("fs");
+const util = require("util");
 const inquirer = require("inquirer");
-const axios 
-const generateMarkdown = require("./utils/generateMarkdown");
-// const writeFileAsync = 
+// const writeFileAsync =
 // array of questions for user
 const questions = [
-    {
-        type: "input",
-        name: "username",
-        message: "What is your GitHub username?",
-        validate: validateInput,
-    },
-    {   // breakdown of components
-        // note type
-        type: "input",
-        // note name
-        name: "title",
-        // note message that will be deployed
-        message: "What will this project be titled?",
-        // note what will be submitted to those who input data to be entered
-        validate: validateInput,
-    },
-    // project description question
-    {
-        type: "input",
-        name: "description",
-        message: "Please enter the description of your project.",
-        validate: validateInput,
-    },
-    
-    // begin TABLE OF CONTENTS in markdown
-
-    // questions for installation
-    {
-        type: "input",
-        name: "installation",
-        message: "Please explain how to install the software and/or commands for the program",
-        validate: validateInput,
-    },
-    // project usage
-    {
-        type: "input",
-        name: "description",
-        message: "Please explain how this project can be used.",
-        validate: validateInput,
-    },
-    // generate list of options for Licensure for ReadMe
-    {
-        type: "list",
-        name: "license",
-        message: "Chose the appropriate license for this project: ",
-        choices: [
-            "Apache",
-            "Academic",
-            "GNU",
-            "ISC",
-            "MIT",
-            "Mozilla",
-            "Open"
-        ],
-        validate: validateInput,
-    },
-    // question for contributing
-    {
-        type: "input",
-        name: "contributing",
-        message: "How can users contribute to this project?",
-        validate: validateInput,
-    },
-    // question for tests
-    {
-        type: "input",
-        name: "tests",
-        message: "Please enter any pertinent testing information regarding this project.",
-        validate: validateInput,
-    },
-    // questions to identify creator  
-    // enter username
-
-
-    // enter email associated with GitHub
-    {
-        type: "input",
-        name: "email",
-        message: "Please enter your email:",
-        validate: validateInput,
-    }
+  {
+    type: "Input",
+    message: "What's the title of your project?",
+    name: "Title",
+  },
+  {
+    type: "Input",
+    message: "What's the description of your project?",
+    name: "Description",
+  },
+  {
+    type: "Input",
+    message: "What's in the table of contents??",
+    name: "ToC",
+  },
+  {
+    type: "Input",
+    message: "What do you need to install?",
+    name: "Install",
+  },
+  {
+    type: "Input",
+    message: "What's the usage of your project?",
+    name: "Usage",
+  },
+  {
+    type: "Input",
+    message: "What's the licensing of your project?",
+    name: "License",
+  },
+  {
+    type: "Input",
+    message: "Who's contributing to your project?",
+    name: "Contribute",
+  },
+  {
+    type: "Input",
+    message: "What's your project testing?",
+    name: "Test",
+  },
+  {
+    type: "Input",
+    message: "What questions are your project asking?",
+    name: "Question",
+  },
+  {
+    type: "Input",
+    message: "What's your github username?",
+    name: "Username",
+  },
+  {
+    type: "Input",
+    message: "What's your Email?",
+    name: "Email",
+  },
+  //  Contributing, Tests, questions
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, generateMarkdown(data), function (err){
-        if (err){
-            return console.log(err);
-        }
-        console.log("Success!");
-    })
+  fs.writeFile(fileName, data, function (err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("Success");
+  });
 }
 
 // function to initialize program
-function init() {
-    inquirer.prompt(questions).then(data){
-        writeToFile(".README.md", data);
-    }
-}
+function init() {}
 
 // function call to initialize program
 init();
